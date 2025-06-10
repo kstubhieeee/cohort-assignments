ec01bee310419bb6852236952cbb5989d3af97d1
kstubhie <banekaustubh27@gmail.com>
2025-06-10 23:51:47 +0530

    easy solved

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -4,8 +4,26 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+/*
+In this approach, I first split the strings into arrays. Then, I sort the characters in each array alphabetically from A to Z. 
+After that, I join the sorted characters back into strings. If the resulting strings match, they are anagrams; otherwise, they are not.
+*/
 
+function isAnagram(str1, str2) {
+  let tracker = true;
+
+  if (str1.length != str2.length) {
+    return false;
+  }
+
+  if (str1.split("").sort().join("") == str2.split("").sort().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+let result = isAnagram("spar", "rasp");
+console.log(result);
 
 module.exports = isAnagram;
--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -14,7 +14,69 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let arr = [];
+  for (let i = 0; i < transactions.length; i++) {
+    let tracker = false;
+    let obj = transactions[i];
+    let category = obj.category;
+    let totalSpent = obj.price;
+
+    for (let j = 0; j < arr.length; j++) {
+      let arrObj = arr[j];
+      if (arrObj.category == category) {
+        arrObj.totalSpent = arrObj.totalSpent + totalSpent;
+        tracker = true;
+        break;
+      }
+    }
+    if (tracker == false) {
+      arr.push({
+        category: category,
+        totalSpent: totalSpent,
+      });
+    }
+  }
+  return arr;
 }
+
+let finalResult = calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: "Clothing",
+    itemName: "T-Shirt",
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: "Electronics",
+    itemName: "Headphones",
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: "Clothing",
+    itemName: "Jeans",
+  },
+]);
+
+console.log(finalResult);
 
 module.exports = calculateTotalSpentByCategory;
